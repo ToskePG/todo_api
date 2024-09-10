fd02484768930c6ec30aa9c450ea8d0acc6c8c12
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-09-09 19:10:13 -0700

    Creates root

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return JSONResponse(
+        status_code=200,
+        content={"message": "Welcome to the TODO API"}
+    )
